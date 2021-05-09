2012e9f5df3da94a1ae10a84eaf4bb6351f93374
kkedzierski <kamil.kedzierski@onet.com.pl>
2021-05-09 21:02:25 +0200

    create email sender

---
+++ email_sender.php
@@ -0,0 +1,27 @@
+<?php
+
+    $message_send = false;
+    if( isset($_POST['sender-email']) && $_POST['sender-email'] != ''){
+        
+        if( filter_var($_POST['subject-email'], FILTER_VALIDATE_EMAIL) ){
+
+            $sender = $_POST['sender-email'];
+            $subject = $_POST['subject-email'];
+            $content = $_POST['content-email'];
+            
+            $to = 'kamil.kedzierski@onet.com.pl';
+            $body = "";
+            $body .= "Cześć Magda, dostałaś wiadomość z portalu matekst.pl \r\n";
+            $body .= "Wiadomość została wysłana od: ".$sender. "\r\n";
+            $body .= "Oto treść wiadomości: ".$sender. "\r\n";
+            
+            mail($to, $subject, $body);
+
+            $message_send = true;
+        }
+
+
+    }
+
+
+?>
