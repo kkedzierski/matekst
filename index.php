e27f45178b8f3f48c00ceb8ed763536a8850fdf4
kkedzierski <kamil.kedzierski@onet.com.pl>
2021-05-10 22:02:55 +0200

    add js type definition to cnd\s

--- index.php
+++ index.php
@@ -16,10 +16,11 @@
     <script
     src="https://code.jquery.com/jquery-3.6.0.min.js"
     integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-    crossorigin="anonymous"></script>
+    crossorigin="anonymous"
+    type="text/javascript"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.5/css/lightslider.min.css" rel="stylesheet" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.5/js/lightslider.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.5/js/lightslider.min.js"></script>
     
     <title>Matekst</title>
 </head>
@@ -114,6 +115,6 @@
         <p><a href="mailto:magda.klosek90@gmail.com">magda.klosek90@gmail.com</a></p>
     </footer>
                  
-    <script src="./src/js/main-script.js"></script>
+    <script type="text/javascript" src="./src/js/main-script.js"></script>
 </body>
 </html>
